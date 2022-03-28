8a26ce4f801001e488235a8a3b24ff500620651f
Ariel Kerzhner <arielk2356@gmail.com>
2022-03-28 15:44:47 +0300

    solved hw0qA.cpp

--- hw0/hw0qA.cpp
+++ hw0/hw0qA.cpp
@@ -1,18 +1,39 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
 #include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
 
 using namespace std;
 
 int main() {
-    int W = 0;
-    cin >> W;
-    int N = 0;
-    cin >> N;
-    int area = 0;
-    for (int i = 0; i < N; i++) {
-        int w = 0, l = 0;
-        cin >> w >> l;
-        area += w * l;
+    ull W = 0;
+    while (cin >> W) {
+        ull N = 0;
+        cin >> N;
+        ll area = 0;
+        for (int i = 0; i < N; i++) {
+            ull w = 0, l = 0;
+            cin >> w >> l;
+            area += w * l;
+        }
+        cout << area / W << endl;
     }
-    cout << area / W << endl;
     return 0;
 }
