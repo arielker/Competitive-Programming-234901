ede07eb568559505f6187e775dfc69ed7fcee0c4
Ariel Kerzhner <arielk2356@gmail.com>
2022-04-10 02:08:37 +0300

    infinite loop in uva_10385.cpp

--- hw1/uva_10385.cpp
+++ hw1/uva_10385.cpp
@@ -18,6 +18,7 @@
 #include <stack>
 #include <string>
 #include <vector>
+#include "iomanip"
 
 using namespace std;
 
@@ -25,8 +26,80 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
 typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
 
-int main(){
+///first is running speed (r) and then cycling speed (k)
+
+constexpr double EPS = 1e-8;
+
+typedef pair<double, double> pdd;
+
+int n;
+double t;
+pdd speeds[20];
+
+double getTime(pdd speed, pdd length) {
+    return length.first / speed.first + length.second / speed.second;
+}
+
+double getDiff(double l) {
+    pdd currentLength(l, t - l);
+    double cheaterTime = getTime(speeds[n - 1], currentLength);
+
+    vector<double> times;
+    times.reserve(n - 1);
+    for (int i = 0; i < n - 1; ++i) {
+        times.emplace_back(getTime(speeds[i], currentLength));
+    }
+
+    return (cheaterTime - *min_element(times.begin(), times.end())) * 3600.00;
+}
+
+int main() {
+    cout << fixed << setprecision(2);
+
+    while (cin >> t) {
+        cin >> n;
+        for (auto &i: speeds) {
+            cin >> i.first >> i.second;
+        }
+
+        ///ternary search
+        double l = 0;
+        double r = t;
+
+        while (fabs(r - l) > EPS) {
+            double ml = (r + r + l) / 3.0;
+            double mr = (r + l + l) / 3.0;
+
+            getDiff(mr) < getDiff(ml) ? (r = ml) : (l = mr);
+        }
+
+        double b = getDiff(l);
+        if (b > 0) {
+            cout << "The cheater cannot win." << endl;
+        } else {
+            cout << "The cheater can win by "
+                 << (int) round(-b)
+                 << " seconds with r = " << l << "km and k = " << t - l
+                 << "km." << endl;
+        }
+    }
 
     return 0;
 }
+
+/*
+100
+3
+10.0 40.0
+20.0 30.0
+15.0 35.0
+
+100
+3
+10.0 40.0
+20.0 30.0
+15.0 25.0
+
+*/
