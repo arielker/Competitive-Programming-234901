e2640a48eee782727359a095a336d235fdaad8e8
Ariel Kerzhner <arielk2356@gmail.com>
2022-04-13 15:46:34 +0300

    code reformat

--- class4/uva_11388.cpp
+++ class4/uva_11388.cpp
@@ -1,37 +1,37 @@
-    #include <algorithm>
-    #include <bitset>
-    #include <cmath>
-    #include <cstdio>
-    #include <cstdlib>
-    #include <cstring>
-    #include <deque>
-    #include <functional>
-    #include <iostream>
-    #include <map>
-    #include <numeric>
-    #include <queue>
-    #include <set>
-    #include <stack>
-    #include <string>
-    #include <vector>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
 
-    using namespace std;
+using namespace std;
 
-    typedef long long ll;
-    typedef unsigned long long ull;
-    typedef vector<int> vi;
-    typedef pair<int, int> pii;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
 
-    int main() {
-        ll t;
-        cin >> t;
-        for (ll i = 0; i < t; i++) {
-            ll g, l;
-            cin >> g >> l;
-            if (l % g == 0)
-                cout << g << " " << l << endl;
-            else
-                cout << "-1" << endl;
-        }
-        return 0;
+int main() {
+    ll t;
+    cin >> t;
+    for (ll i = 0; i < t; i++) {
+        ll g, l;
+        cin >> g >> l;
+        if (l % g == 0)
+            cout << g << " " << l << endl;
+        else
+            cout << "-1" << endl;
     }
+    return 0;
+}
