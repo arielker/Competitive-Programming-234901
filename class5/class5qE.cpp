2b9b5b49f07f75bd72b2136396e672990148884b
Ariel Kerzhner <arielk2356@gmail.com>
2022-04-26 16:56:32 +0300

    added graphImplementations.cpp

--- class5/class5qE.cpp
+++ class5/class5qE.cpp
@@ -23,6 +23,14 @@ typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef set<ll> sll;
 typedef pair<int, int> pii;
+typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
+typedef vector<ii> vii;
+typedef vector<vii> vvii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef set<int> si;
+typedef vector<si> vsi;
 
 int main(){
 
