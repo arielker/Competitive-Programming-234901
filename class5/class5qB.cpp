2675d76f0e7e36dca2fabc730fd998b08a87b5b1
Ariel Kerzhner <arielk2356@gmail.com>
2022-04-26 16:54:42 +0300

    init class5

---
+++ class5/class5qB.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<ll> sll;
+typedef pair<int, int> pii;
+
+int main(){
+
+    return 0;
+}
