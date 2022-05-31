8ea043b6835badf975956360c7da89b9d8bcd797
Ariel Kerzhner <arielk2356@gmail.com>
2022-05-31 16:44:16 +0300

    init class9

---
+++ class9/class9qB.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<ll> sll;
+typedef pair<int, int> pii;
+typedef pair<int, int> ii;
+typedef pair<double, double> pdd;
+typedef pair<int, ii> iii;
+typedef vector<ii> vii;
+typedef vector<vii> vvii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef set<int> si;
+typedef vector<si> vsi;
+
+const int INF = 1e9;
+#define EPS = 1e-9;
+
+int main() {
+
+    return 0;
+}
