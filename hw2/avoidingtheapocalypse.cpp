7d15f7ed81ce820a16cdcf1a8724e7e3bcb0c3c1
Ariel Kerzhner <arielk2356@gmail.com>
2022-06-17 20:45:57 +0300

    finish avoidingtheapocalypse.cpp

--- hw2/avoidingtheapocalypse.cpp
+++ hw2/avoidingtheapocalypse.cpp
@@ -36,15 +36,12 @@ typedef set<int> si;
 typedef vector<si> vsi;
 
 const int INF = 1e9;
-
-void bfs(const vector<vii> &g, int i, map<ii, int> &fixed, int ttl, map<ii, int> &flows);
-
 #define EPS 1e-9
 
 /********** Max Flow **********/
 
 
-int augment(vvi &res, int s, int t, const vi &p, int minEdge) {
+int augment(vector<map<int, int>> &res, int s, int t, const vi &p, int minEdge) {
     // traverse the path from s to t according to p.
     // change the residuals on this path according to the min edge weight along this path.
     // return the amount of flow that was added.
@@ -63,9 +60,8 @@ int augment(vvi &res, int s, int t, const vi &p, int minEdge) {
 //        edges v1->v2 of the form (weight,(v1,v2)), source (s) and target (t).
 // output: max flow from s to t over the edges.
 // time: O(VE^2) and O(EF).
-int EdmondsKarp(int n, vector<iii> &edges, int s, int t) {
-    // initialise adjacenty list and residuals adjacency matrix
-    vvi res(n, vi(n, 0));
+ull EdmondsKarp(ull n, vector<iii> &edges, int s, int t, vector<map<int, int>> &res) {
+    // initialise adjacency list and residuals adjacency matrix
     vvi adj(n);
     for (iii e: edges) {
         res[e.second.first][e.second.second] += e.first;
@@ -99,22 +95,18 @@ int EdmondsKarp(int n, vector<iii> &edges, int s, int t) {
     return maxFlow;
 }
 
-int fixedFlow(int ttl, int w, int passed, int c) {
-    int x = ttl + 1 - w - passed;
-    return x < 0 ? 0 : x * c;
-}
-
 int main() {
     int testcases;
     cin >> testcases;
     while (testcases--) {
         int n, init_location, group, time_to_live, m;
         cin >> n >> init_location >> group >> time_to_live >> m;
+        init_location--;
         si medical_places;
         for (int i = 0; i < m; ++i) {
             int x;
             cin >> x;
-            medical_places.insert(x);
+            medical_places.insert(--x);
         }
         int roads;
         cin >> roads;
@@ -123,61 +115,45 @@ int main() {
             continue;
         }
         vector<vii> weighted_graph(n + 1);
-        map<ii, int> flows;
+        map<ii, ii> flows;
         for (int i = 0; i < roads; ++i) {
             int a, b, p, t;
             cin >> a >> b >> p >> t;
-            auto nodes = make_pair(a, b);
-            weighted_graph[a].emplace_back(b, t);
-            flows[nodes] = p;
+            auto nodes = make_pair(--a, --b);
+            flows[nodes] = make_pair(p, t);
         }
         if (medical_places.find(init_location) != medical_places.end()) {
             cout << group << endl;
             continue;
         }
-        map<ii, int> fixed_flows;
-        bfs(weighted_graph, init_location, fixed_flows, time_to_live, flows);
+        int num_vertices = n * (time_to_live + 1) + 2;
+        int source = n * (time_to_live + 1), target = n * (time_to_live + 1) + 1;
         vector<iii> edges;
-        for (const auto &ff: fixed_flows) {
-            ii nodes = ff.first;
-            int flow = ff.second;
-            edges.emplace_back(flow, nodes);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < time_to_live; ++j) {
+                edges.emplace_back(INF, make_pair(i + j * n, i + (j + 1) * n));
+            }
         }
         for (const auto &i: medical_places) {
-            edges.emplace_back(INF, make_pair(i, n + 1));
+            for (int j = 0; j < time_to_live + 1; ++j)
+                edges.emplace_back(INF, make_pair(i + j * n, target));
         }
-        edges.emplace_back(group, make_pair(0, init_location));
-        cout << EdmondsKarp(n + 2, edges, 0, n + 1) << endl;
+        for (const auto &i: flows) {
+            int a = i.first.first, b = i.first.second, p = i.second.first, t = i.second.second;
+            for (int j = 0; j < time_to_live + 1; ++j) {
+                if (time_to_live < j + t) {
+                    break;
+                }
+                edges.emplace_back(p, make_pair(a + j * n, b + (j + t) * n));
+            }
+        }
+        edges.emplace_back(group, make_pair(source, init_location));
+        vector<map<int, int>> res(num_vertices);
+        cout << EdmondsKarp(num_vertices, edges, source, target, res) << endl;
     }
     return 0;
 }
 
-void bfs(const vector<vii> &g, int i, map<ii, int> &fixed, int ttl, map<ii, int> &flows) {
-    queue<int> q;
-    q.push(i);
-    int update = q.size(), layer = 0;
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        update--;
-        for (const auto &p: g[u]) {
-            int v = p.first, w = p.second;
-            ii nodes = make_pair(u, v);
-            if (fixed.find(nodes) != fixed.end()) {
-                continue;
-            }
-            int flow = fixedFlow(ttl, w, layer, flows[nodes]);
-            if (flow > 0) {
-                fixed[nodes] = flow;
-                q.push(v);
-            }
-        }
-        if (0 == update) {
-            layer++;
-            update = q.size();
-        }
-    }
-}
 /*
 2
 4
@@ -204,4 +180,14 @@ void bfs(const vector<vii> &g, int i, map<ii, int> &fixed, int ttl, map<ii, int>
 3 4 1 3
     --- 8
         9
+
+1
+3
+1 10 5
+1
+3
+2
+1 2 2 1
+2 3 100 1
+    --- 8
  */
